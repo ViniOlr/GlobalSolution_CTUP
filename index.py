eb0eb298cb8f09d1a165fc365f84067309432c08
Vinicius Oliveira <vinioliveiracontato03@gmail.com>
2022-11-01 00:25:11 -0300

    Opção 1 feita

--- index.py
+++ index.py
@@ -1,23 +1,32 @@
-from cv2 import repeat
-
-
-registro = {
+registro = [{
     'mes_ano_referencia' : '10-2022',
     'total_habitantes' : 1000000,
     'total_obitos' : 20000
-}
+    }
+]
 
 def menuPrincipal():
     print("**-- Menu Principal --**\n1 – Cadastrar mês de referência\n2 – Exibir dados do mês de referência [pesquisa por mês]\n3 – Relatório comparativo – Referência 2019\n4 – Listar todos os meses cadastrados")
     escolha = input("Digite a opção desejada: ")
     return int(escolha)
 
+
+def cadastrandoMesAno(mesAno, tHab, tObi):
+    newDict = dict(zip(['mes_ano_referencia', 'total_habitantes', 'total_obitos'], [mesAno, tHab, tObi]))
+    registro.append(newDict)
+    print("***** Gravado com sucesso *****")
+
 repeat = True
 
 while repeat:
     escolha = menuPrincipal()
     if escolha == 1:
-        print("Opção número 1")
+        repeatEscolha1 = True
+        mesAno = input("Mês-ano..............: ")
+        tHab   = input("Total de Habitantes..: ")
+        tObi   = input("Total de óbitos......: ")
+        cadastrandoMesAno(mesAno, tHab, tObi)
+
         aux = input("Deseja continuar? (sim ou nao): ")
         if aux == 'nao':
             repeat = False
@@ -38,3 +47,5 @@ while repeat:
             repeat = False
     else:
         print("\n\tOpção inválida, escolha entre 1, 2 ,3 ou 4\n")
+
+print(registro)
